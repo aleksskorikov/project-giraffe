f308ea6ad9847b4472fb75675a37f4e02afecf17
aleksskorikov <aleksandrskorikov123@gmail.com>
2024-02-10 17:30:33 +0200

    add corrections

--- scripts/script.js
+++ scripts/script.js
@@ -21,30 +21,8 @@ $(document).ready(function () {
     });
 });
 
-// $(document).ready(function () {
-// 	$(".section__reviews-slider").slick({
-// 		// dots: true,
-// 		slidesToShow: 1,
-// 		// centerMode: true,
-// 		responsive: [
-// 		{
-// 			breakpoint: 1100,
-// 			settings: {
-// 				slidesToShow: 2,
-// 			}
-// 		},
-// 		{
-// 			breakpoint: 800,
-// 			settings: {
-// 				slidesToShow: 1,
-// 			}
-// 			},
-// 	]
-// 	});
-// })
 
 new Swiper('.section__reviews-slider', {
-// Navigation arrows
 navigation: {
 nextEl: '.swiper-button-next',
 prevEl: '.swiper-button-prev',
